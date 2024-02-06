2b0fc2401d6e8931a646bfbb8d033abc763a2638
Takumi <thatallql@gmail.com>
2024-02-06 20:11:14 +0300

    3 files changed, it's almost works.

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ import random
 config = json.load(open("isthatright.json"))
 token = config["config"]["token"]
 
-url = f"https://api.telegram.org/bot{token}/" #f"https://api.telegram.org/bot{token}/"
+url = f"https://api.telegram.org/bot{token}/" 
 frogs_list = "https://drive.google.com/drive/folders/1vKe7Up1EMEaPS1EPlfTYRE91ApvUGbyH"
+
 
 while True:
     with open("lastMessage") as f:
@@ -28,13 +29,29 @@ while True:
             username = update["message"]["from"]["first_name"]
         if not username:
             username = "anon"
-
-        if txt.strip().lower() == "hello":
+            
+        # if txt.strip().lower() == "k-index":
+        #     k_indexes = requests.get(k_index)
+        #     last_k_index = k_indexes.json()[-1]
+        #     requests.post(url+"sendMessage", data={
+        #         "chat_id": update["message"]["chat"]["id"],
+        #         "text": f"Currently K-Index is {last_k_index[1]}"
+        #     })
+                
+        elif txt.strip().lower() == "hello":
             requests.post(url+"sendMessage", data={
                 "chat_id": update["message"]["chat"]["id"],
-                "text": f"Hello, {username}, my dude. I'm your friendly wednesday reminder!"
+                "text": f"Hello, {username}. I'm here to hail all Wednesdudes!"
             })
+
+        else:
+            requests.post(url+"sendMessage", data={
+                "chat_id": update["message"]["chat"]["id"],
+                "text": f"Sorry, don't get it."
+            })
+
         with open("lastMessage", "w") as f:
             f.write(str(update["update_id"]))
-            
-time.sleep(1)
+
+
+    time.sleep(1)
